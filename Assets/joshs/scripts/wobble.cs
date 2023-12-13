92af3ffac7f1c43aea9c6cfc1dfae33bbff6535f
Dahl <1717734@mukilteo.wednet.edu>
2023-12-13 12:26:08 -0800

    layer and the start to the enemy wobble

---
+++ Assets/joshs/scripts/wobble.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wobble : MonoBehaviour
+{
+    [Range(0.1f, 5f)]
+    public float WaitBetweenWobbles = 0.5f;
+
+   [Range(1f,50f)]
+    public float Intensity = 10f;
+
+    Quaternion _targetAngle;
+   
+    
+    void start()
+    {
+        InvokeRepeating("ChangeTarget", 0, WaitBetweenWobbles);
+    }
+    void Update()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, _targetAngle, Time.deltaTime);
+    }
+    void ChangeTarget()
+    {
+        var Intensity = Random.Range(0.1f, Intensity);
+        var curve = Mathf.Sin(Random.Range(0, Mathf.PI * 2));
+
+        _targetAngle = Quaternion.Euler(Vector3.forward * curve * Intensity);
+    }
+}
