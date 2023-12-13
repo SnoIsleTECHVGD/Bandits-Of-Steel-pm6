17702c5ba3f3982e873bfbf1e994445b49510a05
Edwards <1717469@mukilteo.wednet.edu>
2023-12-13 13:21:28 -0800

    Things transition

---
+++ Assets/Unit.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+
+    public string unitName;
+    public int unitLevel;
+
+    public int damage;
+
+    public int maxHP;
+    public int currentHP;
+
+
+
+
+
+}
