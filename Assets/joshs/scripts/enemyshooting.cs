2c562c60ebcd70a6890b3a86633ee6b7fbcb2b09
Dahl <1717734@mukilteo.wednet.edu>
2024-01-09 13:23:31 -0800

    enemy shoot range and working of health bar

--- Assets/joshs/scripts/enemyshooting.cs
+++ Assets/joshs/scripts/enemyshooting.cs
@@ -6,26 +6,34 @@ public class enemyshooting : MonoBehaviour
 {
     public  GameObject bullet;
         public Transform bulletpos;
-
+    public float distance;
+    public float distanceBetween;
     private float timer;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameObject.Find("player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+        distance = Vector2.Distance(transform.position, player.transform.position);
+        Vector2 direction = player.transform.position - transform.position;
+        direction.Normalize();
 
-        if(timer > 2)
+        if (distance < distanceBetween)
         {
-            timer = 0;
-            shoot();
+            timer += Time.deltaTime;
+
+            if (timer > 2)
+            {
+                timer = 0;
+                shoot();
+            }
         }
-  
-        
+
     }
     void shoot()
     {
