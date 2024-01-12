b62c1cce17969088e1681feea66d8e9f2c4ad0cc
Grigsby <1717646@mukilteo.wednet.edu>
2024-01-12 11:47:05 -0800

    fixing merge conflicts

--- Assets/Animation_Control.cs
+++ Assets/Animation_Control.cs
@@ -5,17 +5,20 @@ using UnityEngine;
 
 public class Animation_Control : MonoBehaviour
 {
- 
+    Animator anim;
     // Start is called before the first frame update
     void Start()
     {
-   
+        anim = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     
+     if (Input.GetKeyDown(KeyCode.S))
+        {
+            anim.SetTrigger("Run Down");
+        }
     }
 
  
