dfdeaaa2462e1c698a2b22a34975046b7c96e16a
Dahl <1717734@mukilteo.wednet.edu>
2024-01-19 12:03:04 -0800

    fixing prefab

--- Assets/joshs/scripts/EnemyBulletScript.cs
+++ Assets/joshs/scripts/EnemyBulletScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class EnemyBulletScript : MonoBehaviour
 {
-    private GameObject player;
+    private GameObject npc;
     private Rigidbody2D rb;
     public float force;
     public float distance;
@@ -14,9 +14,9 @@ public class EnemyBulletScript : MonoBehaviour
     {
 
         rb = GetComponent<Rigidbody2D>();
-        player = GameObject.FindGameObjectWithTag("Player");
+        npc = GameObject.FindGameObjectWithTag("npc");
 
-        Vector3 direction = player.transform.position - transform.position;
+        Vector3 direction = npc.transform.position - transform.position;
         rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
     }
 
