fdffa6097cc43957fb96e8231f7af9814cb1f7e1
Edwards <1717469@mukilteo.wednet.edu>
2024-01-22 12:59:10 -0800

    fixing things in my copy of game

--- Assets/joshs/scripts/stats.cs
+++ Assets/joshs/scripts/stats.cs
@@ -9,28 +9,9 @@ public class stats : MonoBehaviour
      public int defence;
     public float speed;
     private float bulletForce;
-    public List<ItemList> items = new List<ItemList>();
+   
 
-    void Start()
-    {
-        HealingItem item = new HealingItem();
-        items.Add(new ItemList(item, item.GiveName(), 1));
-        StartCoroutine(CallItemUpdate());
-    }
-
-    void Update()
-    {
-        
-    }
-
-    IEnumerator CallItemUpdate()
-    {
-        foreach (ItemList i in items)
-        {
-            i.item.Update(this, i.stacks);
-        }
-        yield return new WaitForSeconds(1);
-        StartCoroutine(CallItemUpdate());
-    }
+   
+    
 }
 
